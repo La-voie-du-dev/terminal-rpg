ee72910ba026aa5d7844649997b26b64b5aafa3b
David BELBEZE <david.belbeze@gmail.com>
2023-12-20 21:33:08 +0100

    Utilisation de Node dans le programme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TerminalRpg.Game;
+
+Node node = new Node();
+
+node.X = 2;
+node.Y = 2;
+node.Representation = 'o';
+
+Console.WriteLine(
+    "At ({0}, {1}) '{2}'", node.X, node.Y, node.Representation
+);
