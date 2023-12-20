aad050801ba59c92aeca75263ad017b5f3080590
David BELBEZE <david.belbeze@gmail.com>
2023-12-20 21:17:43 +0100

    Déclaration de la classe Node

---
+++ src/Game/Node.cs
@@ -0,0 +1,18 @@
+namespace TerminalRpg.Game {
+    public class Node {
+        /// <summary>
+        /// La colonne où le caractère doit être affiché.
+        /// </summary>
+        public int X = 0;
+
+        /// <summary>
+        /// La ligne où le caractère doit être affiché.
+        /// </summary>
+        public int Y = 0;
+
+        /// <summary>
+        /// Le caractère représentant l'entité.
+        /// </summary>
+        public char Representation;
+    }
+}
