f84ffcc864e300ac3dce550f964ab2adff900eaf
David BELBEZE <david.belbeze@gmail.com>
2023-12-21 10:54:38 +0100

    Ajout du test du traitement par copie ou référence

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 using TerminalRpg.Game;
 
-Node node = new Node();
-
-node.X = 2;
-node.Y = 2;
-node.Representation = 'o';
+Node node = new Node('o', 2, 2);
+Node nRef = node;
 
 Console.WriteLine(
-    "At ({0}, {1}) '{2}'", node.X, node.Y, node.Representation
+    "node Node['{0}', ({1}, {2})] / nRef Node['{3}', ({4}, {5})]",
+    node.Representation, node.X, node.Y,
+    nRef.Representation, nRef.X, nRef.Y
+);
+
+nRef.X = 1;
+nRef.Y = 3;
+nRef.Representation = '#';
+
+Console.WriteLine(
+    "node Node['{0}', ({1}, {2})] / nRef Node['{3}', ({4}, {5})]",
+    node.Representation, node.X, node.Y,
+    nRef.Representation, nRef.X, nRef.Y
 );
