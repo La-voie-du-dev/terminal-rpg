034bdcaf46fe4e97bc6d7f3527fd57e2c3427d77
David BELBEZE <david.belbeze@gmail.com>
2023-12-23 19:24:53 +0100

    Correction des modificateurs d'accès de la classe Node

--- src/Game/Node.cs
+++ src/Game/Node.cs
@@ -1,19 +1,39 @@
 namespace TerminalRpg.Game {
     public class Node {
+        private int _x;
         /// <summary>
         /// La colonne où le caractère doit être affiché.
         /// </summary>
-        public int X = 0;
+        public int X {
+            get { return _x; }
+            set {
+                if (value < 0) {
+                    throw new Exception(
+                        "La valeur minimale acceptée est 0");
+                }
+                _x = value;
+            }
+        }
 
+        private int _y;
         /// <summary>
         /// La ligne où le caractère doit être affiché.
         /// </summary>
-        public int Y = 0;
+        public int Y {
+            get => _y;
+            set {
+                if (value < 0) {
+                    throw new Exception(
+                        "La valeur minimale acceptée est 0");
+                }
+                _y = value;
+            }
+        }
 
         /// <summary>
         /// Le caractère représentant l'entité.
         /// </summary>
-        public char Representation;
+        public char Representation { get; }
 
         public Node(char representation, int X = 0, int Y = 0) {
             this.X = X;
