f2304b84c3fbf461a1ef5838b8327e8ea813ff67
David BELBEZE <david.belbeze@gmail.com>
2023-12-30 17:33:33 +0100

    Déclaration et utilisation de la classe Tree

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ tile.AddNode(new Node('o', 2, 2));
 tile.AddNode(new Node('#', 4, 0));
 tile.AddNode(new Rock(1, 1));
 tile.AddNode(new Rock(3, 4));
-tile.AddNode(new Node('?', 3, 2));
-tile.AddNode(new Node('?', 0, 3));
+tile.AddNode(new Tree(3, 2));
+tile.AddNode(new Tree(0, 3));
 tile.AddNode(new Node('†', 1, 4));
 
 tile.Display();
---
+++ src/Environment/Tree.cs
@@ -0,0 +1,8 @@
+using TerminalRpg.Game;
+
+namespace TerminalRpg.Environment {
+    public class Tree : Node
+    {
+        public Tree(int X = 0, int Y = 0) : base('?', X, Y) { }
+    }
+}
