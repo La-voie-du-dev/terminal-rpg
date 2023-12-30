e85590848e472096fc0ea6a39fa89fcabec885aa
David BELBEZE <david.belbeze@gmail.com>
2023-12-30 17:23:15 +0100

    Déclaration et utilisation de la classe Rock

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using TerminalRpg.Environment;
 using TerminalRpg.Game;
 
 Tile tile = new Tile();
@@ -5,8 +6,8 @@ Tile tile = new Tile();
 tile.AddNode(new Node('o', 1, 0));
 tile.AddNode(new Node('o', 2, 2));
 tile.AddNode(new Node('#', 4, 0));
-tile.AddNode(new Node('.', 1, 1));
-tile.AddNode(new Node('.', 3, 4));
+tile.AddNode(new Rock(1, 1));
+tile.AddNode(new Rock(3, 4));
 tile.AddNode(new Node('?', 3, 2));
 tile.AddNode(new Node('?', 0, 3));
 tile.AddNode(new Node('†', 1, 4));
---
+++ src/Environment/Rock.cs
@@ -0,0 +1,8 @@
+using TerminalRpg.Game;
+
+namespace TerminalRpg.Environment {
+    public class Rock : Node
+    {
+        public Rock(int X = 0, int Y = 0) : base('.', X, Y) { }
+    }
+}
